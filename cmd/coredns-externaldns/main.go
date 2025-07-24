4bbe2cba886a541dacbd4b5801006eaa783469b5
Seena Fallah <seena.fallah@ionos.com>
2025-07-24 21:25:34 +0200

    refactor: always insert plugin after etcd
    Signed-off-by: Seena Fallah <seena.fallah@ionos.com>

--- cmd/coredns-externaldns/main.go
+++ cmd/coredns-externaldns/main.go
@@ -16,63 +16,18 @@ import (
 )
 
 func init() {
-	// Set complete plugin order - using available plugins from CoreDNS v1.12.2
-	dnsserver.Directives = []string{
-		"root",
-		"metadata",
-		"geoip",
-		"cancel",
-		"tls",
-		"quic",
-		"timeouts",
-		"multisocket",
-		"reload",
-		"nsid",
-		"bufsize",
-		"bind",
-		"debug",
-		"trace",
-		"ready",
-		"health",
-		"pprof",
-		"prometheus",
-		"errors",
-		"log",
-		"dnstap",
-		"local",
-		"dns64",
-		"acl",
-		"any",
-		"chaos",
-		"loadbalance",
-		"tsig",
-		"cache",
-		"rewrite",
-		"header",
-		"dnssec",
-		"autopath",
-		"minimal",
-		"template",
-		"transfer",
-		"hosts",
-		"route53",
-		"azure",
-		"clouddns",
-		"k8s_external",
-		"kubernetes",
-		"file",
-		"auto",
-		"secondary",
-		"etcd",
-		"externaldns", // Our plugin comes here
-		"loop",
-		"forward",
-		"grpc",
-		"erratic",
-		"whoami",
-		"on",
-		"sign",
-		"view",
+	// Find etcd and insert externaldns plugin after it
+	inserted := false
+	for i, dir := range dnsserver.Directives {
+		if dir == "etcd" {
+			dnsserver.Directives = append(dnsserver.Directives[:i+1], "externaldns")
+			inserted = true
+			break
+		}
+	}
+
+	if !inserted {
+		panic("etcd directive not found in CoreDNS directives")
 	}
 }
 
